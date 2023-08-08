decf24a367368bc1f1cab6eaddd6ec59dca27cf4
allan lenny <owinoallan77@gmail.com>
2023-08-08 17:14:12 +0300

    timestamps

--- models/Product.js
+++ models/Product.js
@@ -8,6 +8,8 @@ const ProductSchema = new Schema({
   images: [{ type: String }],
   category: { type: mongoose.Types.ObjectId, ref: 'Category' },
   properties: {type:Object}
+}, {
+  timestamps: true,
 })
 
  export const Product = models.Product ||  model('Product', ProductSchema)
